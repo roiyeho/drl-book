bdd70a2cbbf45c83784f6894b1aa2a126f3179f3
Roi Yehoshua <r.yehoshua@northeastern.edu>
2020-07-11 16:47:55 -0400

    Small changes in the monitor example

--- chapter03/monitor_demo.py
+++ chapter03/monitor_demo.py
@@ -2,19 +2,23 @@
 # Date: June 2020
 import gym
 
-def make_video(env, max_episode_len=100):
-    # Add a Monitor wrapper for recording a video
+def make_video(env, max_episode_len=500):
+    """Record a video of a random agent acting in the given environment
+    :param env: an instance of a gym environment
+    :param max_episode_len: maximum number of steps to run
+    """
+    # Wrap the environment in side a Monitor
     env = gym.wrappers.Monitor(env, f'videos/{env.spec.id}')
-    env.reset()
 
     # Run one episode on the wrapped environment
+    env.reset()
     for step in range(max_episode_len):
         action = env.action_space.sample()
         observation, reward, done, info = env.step(action)
         if done:
             break
-
     print(f'Finished recording the policy')
 
-env = gym.make('CartPole-v0')
-make_video(env)
+if __name__ == '__main__':
+    env = gym.make('Breakout-v4')
+    make_video(env)
