96a5fa59334cd0f13923d119097e74f4123cedec
Roi Yehoshua <r.yehoshua@northeastern.edu>
2020-07-19 15:50:19 -0400

    Removing bandits code from chapter 5

--- chapter05/mc/computing_pi.py
+++ chapter05/mc/computing_pi.py
@@ -1,25 +1,34 @@
 import numpy as np
 
-def within_circle(x, y):
-    R = 0.5
-    if x ** 2 + y ** 2 < R ** 2:
-        return True
-    else:
-        return False
-
 def compute_pi(n_samples=1000000):
-    n = 0
-    n_inside = 0
+    """Estimate the value of pi by sampling points inside a unit square and
+    checking how many of them fall inside the enclosed circle
+    :param n_samples: number of samples to use
+    :return: estimated value of pi
+    """
+    n, n_inner = 0, 0
 
     for i in range(n_samples):
         x = np.random.random() - 0.5
         y = np.random.random() - 0.5
-        if within_circle(x, y):
-            n_inside += 1
+        if is_inside_circle(x, y):
+            n_inner += 1
         n += 1
 
-    pi = 4 * n_inside / n
+    pi = 4 * n_inner / n
     return pi
+
+def is_inside_circle(x, y):
+    """Check if a given point falls inside the enclosed circle
+    :param x: x coordinate of the point
+    :param y: y coordinate of the point
+    :return: a boolean True/False
+    """
+    r = 0.5  # radius of the circle
+    if x ** 2 + y ** 2 < r ** 2:
+        return True
+    else:
+        return False
 
 if __name__ == '__main__':
     pi = compute_pi()
